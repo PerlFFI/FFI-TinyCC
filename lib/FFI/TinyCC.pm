e065fd0beff2decf83a26bef790a4d94a185360d
Graham Ollis <plicease@cpan.org>
2014-10-27 16:50:27 -0400

    exceptions!

--- lib/FFI/TinyCC.pm
+++ lib/FFI/TinyCC.pm
@@ -61,7 +61,11 @@ use constant _delete => FFI::Raw->new(
   FFI::Raw::ptr,
 );
 
-# TODO: tcc_set_error_func
+use constant _set_error_func => FFI::Raw->new(
+  _lib, 'tcc_set_error_func',
+  FFI::Raw::void,
+  FFI::Raw::ptr, FFI::Raw::ptr, FFI::Raw::ptr,
+);
 
 use constant _add_incude_path => FFI::Raw->new(
   _lib, 'tcc_add_include_path',
@@ -178,17 +182,45 @@ Create a new TinyCC instance.
 sub new
 {
   my($class) = @_;
-  bless {
+  
+  my $self = bless {
     handle   => _new->call,
     relocate => 0,
+    error    => [],
   }, $class;
+  
+  $self->{error_cb} = FFI::Raw::Callback->new(
+    sub { push @{ $self->{error} }, $_[1] },
+    FFI::Raw::void,
+    FFI::Raw::ptr, FFI::Raw::str,
+  );
+  
+  _set_error_func->call($self->{handle}, undef, $self->{error_cb});
+  
+  $self;
+}
+
+sub _error
+{
+  my($self, $msg) = @_;
+  push @{ $self->{error} }, $msg;
+  $self;
 }
 
 sub DESTROY
 {
   my($self) = @_;
-  _free->call($self->{store}) if defined $self->{store};
-  _delete->call($self->{handle});
+
+  # weird things happen during global distruction.  The
+  # _delete and _free constants go bye bye sometimes.
+  # since the process is going to end anyway, freeing
+  # the resources for this instance can be skipped.
+  if(ref(_delete) eq 'FFI::Raw')
+  {  
+    _delete->call($self->{handle});
+    # TODO: maybe not a good idea?
+    _free->call($self->{store}) if defined $self->{store};
+  }
 }
 
 =head1 METHODS
@@ -209,8 +241,7 @@ sub add_file
 {
   my($self, $filename) = @_;
   my $r = _add_file->call($self->{handle}, $filename);
-  # TODO: better diagnostic
-  die "unable to add $filename: $!" if $r == -1;
+  die FFI::TinyCC::Exception->new($self) if $r == -1;
   $self;
 }
 
@@ -226,8 +257,7 @@ sub compile_string
 {
   my($self, $code) = @_;
   my $r = _compile_string->call($self->{handle}, $code);
-  # TODO: better diagnostic
-  die "unable to compile" if $r == -1;
+  die FFI::TinyCC::Exception->new($self) if $r == -1;
   $self;
 }
 
@@ -240,7 +270,12 @@ sub compile_string
 sub run
 {
   my($self, @args) = @_;
-  die "unable to use run method after get_symbol" if $self->{relocate};
+  
+  if($self->{relocate})
+  {
+    $self->{error} = ['unable to use run method after get_symbol'];
+    die FFI::TinyCC::Exception->new($self);
+  }
   
   my $argc = scalar @args;
   my @c_strings = map { "$_\0" } @args;
@@ -269,11 +304,65 @@ sub get_symbol
     my $size = _relocate->call($self->{handle}, undef);
     $self->{store} = _malloc->call($size);
     my $r = _relocate->call($self->{handle}, $self->{store});
-    # TODO: better diagnostic
-    die "unable to relocate" if $r == -1;
+    FFI::TinyCC::Exception->new($self) if $r == -1;
     $self->{relocate} = 1;
   }
   _get_symbol->call($self->{handle}, $symbol_name);
+}
+
+package
+  FFI::TinyCC::Exception;
+
+use overload '""' => sub {
+  my $self = shift;
+  if(@{ $self->{fault} } == 2)
+  {
+    join(' ', $self->as_string, 
+      at => $self->{fault}->[0], 
+      line => $self->{fault}->[1],
+    );
+  }
+  else
+  {
+    $self->as_string . "\n";
+  }
+};
+use overload fallback => 1;
+
+sub new
+{
+  my($class, $tcc) = @_;
+  
+  my @errors = @{ $tcc->{error} };
+  $tcc->{errors} = [];
+  my @stack;
+  my @fault;
+  
+  my $i=2;
+  while(my @frame = caller($i++))
+  {
+    push @stack, \@frame;
+    if(@fault == 0 && $frame[0] !~ /^FFI::TinyCC/)
+    {
+      @fault = ($frame[1], $frame[2]);
+    }
+  }
+  
+  my $self = bless {
+    errors => \@errors,
+    stack  => \@stack,
+    fault  => \@fault,
+  }, $class;
+  
+  $self;
+}
+
+sub errors { shift->{errors} }
+
+sub as_string
+{
+  my($self) = @_;
+  join "\n", @{ $self->{errors} };
 }
 
 1;
--- t/compile_string.t
+++ t/compile_string.t
@@ -1,12 +1,16 @@
 use strict;
 use warnings;
 use v5.10;
-use Test::More tests => 2;
+use Test::More tests => 1;
 use FFI::TinyCC;
 
-my $tcc = FFI::TinyCC->new;
+subtest 'basic' => sub {
+  plan tests => 2;
+  my $tcc = FFI::TinyCC->new;
 
-eval { $tcc->compile_string(q{ int main(int argc, char *argv[]) { return 22; } }) };
-is $@, '', 'tcc.compile_string';
+  eval { $tcc->compile_string(q{ int main(int argc, char *argv[]) { return 22; } }) };
+  is $@, '', 'tcc.compile_string';
 
-is $tcc->run, 22, 'tcc.run';
+  is $tcc->run, 22, 'tcc.run';
+};
+
