34060c6b57349793f78c6b43da03760cd971a01e
Graham Ollis <perl@wdlabs.com>
2014-10-28 06:20:13 -0400

    callback example

---
+++ example/callback.pl
@@ -0,0 +1,34 @@
+use strict;
+use warnings;
+use v5.10;
+use FFI::TinyCC;
+use FFI::Raw;
+
+my $say = FFI::Raw::Callback->new(
+  sub { say $_[0] },
+  FFI::Raw::void,
+  FFI::Raw::str,
+);
+
+my $tcc = FFI::TinyCC->new;
+
+$tcc->add_symbol(say => $say);
+
+$tcc->compile_string(q{
+extern void say(const char *);
+
+int
+main(int argc, char *argv[])
+{
+  int i;
+  for(i=1; i<argc; i++)
+  {
+    say(argv[i]);
+  }
+}
+});
+
+# use '-' for the program name
+my $r = $tcc->run('-', @ARGV);
+
+exit $r;
